067bc613b19526290150801a92e4e1a693d2aa61
Nickymonkey <nickroewe@gmail.com>
2020-07-21 15:23:32 -0700

    Fixing Snap Turn + Longbow
    Updated to fix snap turn and longbox

--- Assets/_scripts/_networked/NetworkedSnapTurn.cs
+++ Assets/_scripts/_networked/NetworkedSnapTurn.cs
@@ -6,13 +6,11 @@ using Valve.VR.InteractionSystem;
 
 namespace HPVR
 {
-    public class NetworkedSnapTurn : SnapTurn
+    public class NetworkedSnapTurn : MonoBehaviour
     {
-        private bool canRotate = true;
-
         private void Update()
         {
-            Player player = Player.instance;
+            //Player_VR player = Player_VR.LocalPlayerInstance.GetComponent<Player_VR>();
 
             if (canRotate && snapLeftAction != null && snapRightAction != null && snapLeftAction.activeBinding && snapRightAction.activeBinding)
             {
@@ -31,13 +29,14 @@ namespace HPVR
                 //    (player.leftHand.currentAttachedObject != null
                 //    && player.leftHand.currentAttachedTeleportManager != null
                 //    && player.leftHand.currentAttachedTeleportManager.teleportAllowed);
+                bool rightHandValid = true;
+                bool leftHandValid = true;
 
+                bool leftHandTurnLeft = snapLeftAction.GetStateDown(SteamVR_Input_Sources.LeftHand) && leftHandValid;
+                bool rightHandTurnLeft = snapLeftAction.GetStateDown(SteamVR_Input_Sources.RightHand) && rightHandValid;
 
-                bool leftHandTurnLeft = snapLeftAction.GetStateDown(SteamVR_Input_Sources.LeftHand);
-                bool rightHandTurnLeft = snapLeftAction.GetStateDown(SteamVR_Input_Sources.RightHand);
-
-                bool leftHandTurnRight = snapRightAction.GetStateDown(SteamVR_Input_Sources.LeftHand);
-                bool rightHandTurnRight = snapRightAction.GetStateDown(SteamVR_Input_Sources.RightHand);
+                bool leftHandTurnRight = snapRightAction.GetStateDown(SteamVR_Input_Sources.LeftHand) && leftHandValid;
+                bool rightHandTurnRight = snapRightAction.GetStateDown(SteamVR_Input_Sources.RightHand) && rightHandValid;
 
                 if (leftHandTurnLeft || rightHandTurnLeft)
                 {
@@ -49,5 +48,140 @@ namespace HPVR
                 }
             }
         }
+
+        protected IEnumerator DoRotatePlayer(float angle)
+        {
+            Player_VR player = Player_VR.LocalPlayerInstance.GetComponent<Player_VR>();
+
+            canRotate = false;
+
+            snapTurnSource.panStereo = angle / 90;
+            snapTurnSource.PlayOneShot(rotateSound);
+
+            if (fadeScreen)
+            {
+                SteamVR_Fade.Start(Color.clear, 0);
+
+                Color tColor = screenFadeColor;
+                tColor = tColor.linear * 0.6f;
+                SteamVR_Fade.Start(tColor, fadeTime);
+            }
+
+            yield
+            return new WaitForSeconds(fadeTime);
+
+            Vector3 playerFeetOffset = player.trackingOriginTransform.position - player.feetPositionGuess;
+            player.trackingOriginTransform.position -= playerFeetOffset;
+            player.transform.Rotate(Vector3.up, angle);
+            playerFeetOffset = Quaternion.Euler(0.0f, angle, 0.0f) * playerFeetOffset;
+            player.trackingOriginTransform.position += playerFeetOffset;
+
+            GameObject fx = angle > 0 ? rotateRightFX : rotateLeftFX;
+
+            if (showTurnAnimation)
+                ShowRotateFX(fx);
+
+            if (fadeScreen)
+            {
+                SteamVR_Fade.Start(Color.clear, fadeTime);
+            }
+
+            float startTime = Time.time;
+            float endTime = startTime + canTurnEverySeconds;
+
+            while (Time.time <= endTime)
+            {
+                yield
+                return null;
+                UpdateOrientation(fx);
+            };
+
+            fx.SetActive(false);
+            canRotate = true;
+        }
+
+        public void RotatePlayer(float angle)
+        {
+            if (rotateCoroutine != null)
+            {
+                StopCoroutine(rotateCoroutine);
+                AllOff();
+            }
+
+            rotateCoroutine = StartCoroutine(DoRotatePlayer(angle));
+        }
+
+        protected void ShowRotateFX(GameObject fx)
+        {
+            if (fx == null)
+                return;
+
+            fx.SetActive(false);
+
+            UpdateOrientation(fx);
+
+            fx.SetActive(true);
+
+            UpdateOrientation(fx);
+        }
+
+        protected void UpdateOrientation(GameObject fx)
+        {
+            Player_VR player = Player_VR.LocalPlayerInstance.GetComponent<Player_VR>();
+
+            //position fx in front of face
+            this.transform.position = player.hmdTransform.position + (player.hmdTransform.forward * distanceFromFace);
+            this.transform.rotation = Quaternion.LookRotation(player.hmdTransform.position - this.transform.position, Vector3.up);
+            this.transform.Translate(additionalOffset, Space.Self);
+            this.transform.rotation = Quaternion.LookRotation(player.hmdTransform.position - this.transform.position, Vector3.up);
+        }
+
+        //********************************
+
+        public float snapAngle = 90.0f;
+
+        public bool showTurnAnimation = true;
+
+        public AudioSource snapTurnSource;
+        public AudioClip rotateSound;
+
+        public GameObject rotateRightFX;
+        public GameObject rotateLeftFX;
+
+        public SteamVR_Action_Boolean snapLeftAction = SteamVR_Input.GetBooleanAction("SnapTurnLeft");
+        public SteamVR_Action_Boolean snapRightAction = SteamVR_Input.GetBooleanAction("SnapTurnRight");
+
+        public bool fadeScreen = true;
+        public float fadeTime = 0.1f;
+        public Color screenFadeColor = Color.black;
+
+        public float distanceFromFace = 1.3f;
+        public Vector3 additionalOffset = new Vector3(0, -0.3f, 0);
+
+        public static float teleportLastActiveTime;
+
+        private bool canRotate = true;
+
+        public float canTurnEverySeconds = 0.4f;
+
+
+        private void Start()
+        {
+            AllOff();
+        }
+
+        private void AllOff()
+        {
+            if (rotateLeftFX != null)
+                rotateLeftFX.SetActive(false);
+
+            if (rotateRightFX != null)
+                rotateRightFX.SetActive(false);
+        }
+
+
+
+        private Coroutine rotateCoroutine;
+
     }
 }
