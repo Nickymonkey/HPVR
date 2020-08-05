f69a0233500f19a85f28ec92f64c06b5968ef3b5
Nickymonkey <nickroewe@gmail.com>
2020-08-05 13:39:08 -0700

    Finished up 5th Puzzle and updated pressure plate + portcullis

--- Assets/_scripts/Portcullis.cs
+++ Assets/_scripts/Portcullis.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Portcullis : MonoBehaviour
 {
     public float OpenPosition;
-    public float ClosePosition;
-    public float StartPosition;
+    private float ClosePosition;
+    //public float StartPosition;
     public float timeToLerp;
+    public int triggersNeeded = -1;
+    public int currentNumTriggers = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LerpPosition(new Vector3(transform.position.x, StartPosition, transform.position.z), timeToLerp));
+        ClosePosition = transform.position.y;
+        //StartCoroutine(LerpPosition(new Vector3(transform.position.x, transform.position.y, transform.position.z), timeToLerp));
     }
 
     // Update is called once per frame
@@ -38,12 +41,27 @@ public class Portcullis : MonoBehaviour
     public void Open()
     {
         StopAllCoroutines();
-        StartCoroutine(LerpPosition(new Vector3(transform.position.x, OpenPosition, transform.position.z), timeToLerp));
+        StartCoroutine(LerpPosition(new Vector3(transform.position.x, ClosePosition+OpenPosition, transform.position.z), timeToLerp));
     }
 
     public void Close()
     {
         StopAllCoroutines();
         StartCoroutine(LerpPosition(new Vector3(transform.position.x, ClosePosition, transform.position.z), timeToLerp));
+    }
+
+    public void AddTrigger()
+    {
+        currentNumTriggers++;
+        if (currentNumTriggers == triggersNeeded)
+        {
+            Open();
+        }
+    }
+
+    public void RemoveTrigger()
+    {
+        currentNumTriggers--;        
+        Close();
     }
 }
--- Assets/_scripts/PressurePlate.cs
+++ Assets/_scripts/PressurePlate.cs
@@ -7,6 +7,9 @@ public class PressurePlate : MonoBehaviour
 {
     public bool RequiresPerson = false;
     public bool triggered = false;
+    private bool collisionStay = false;
+    public int numOfCollisions = 0;
+
     [Tooltip("If pressure plate triggered")]
     public UnityEvent onTrigger;
 
@@ -19,48 +22,52 @@ public class PressurePlate : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         
     }
 
-    void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-        if(!triggered)
+        if (RequiresPerson)
         {
-            if (RequiresPerson)
-            {
-                if (collision.collider.gameObject.layer == 8)
-                {
-                    activated();
-                }
-            }
-            else
+            if (collision.collider.gameObject.layer == 8)
             {
                 activated();
             }
+        }
+        else
+        {
+            activated();
         }
     }
 
     private void OnCollisionExit(Collision collision)
     {
-        if (triggered)
-        {
-            deactivated();
-        }
+        deactivated();
     }
 
     void activated()
     {
-        this.gameObject.GetComponent<Renderer>().material.color = Color.green;
-        triggered = true;
-        onTrigger.Invoke();
+        numOfCollisions++;
+        //triggered = true;
+        if (!collisionStay)
+        {
+            collisionStay = true;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.green;
+            onTrigger.Invoke();
+        }
     }
 
     void deactivated()
     {
-        this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
-        triggered = false;
-        offTrigger.Invoke();
+        numOfCollisions--;
+        if (numOfCollisions <= 0 && collisionStay)
+        {
+            collisionStay = false;
+            this.gameObject.GetComponent<Renderer>().material.color = Color.yellow;
+            //triggered = false;
+            offTrigger.Invoke();
+        }
     }
 }
